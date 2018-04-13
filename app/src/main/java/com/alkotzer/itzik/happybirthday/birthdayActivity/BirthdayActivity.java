d47b40155f1665cd53b27dc657bd83a62ae8a118
Itzik Alkotzer <itzik.alkotzer@pixoneye.com>
2018-04-13 18:41:50 +0300

    Adding Birthday activity

---
+++ app/src/main/java/com/alkotzer/itzik/happybirthday/birthdayActivity/BirthdayActivity.java
@@ -0,0 +1,13 @@
+package com.alkotzer.itzik.happybirthday.birthdayActivity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.alkotzer.itzik.happybirthday.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_birthday)
+public class BirthdayActivity extends AppCompatActivity
+{
+
+}
